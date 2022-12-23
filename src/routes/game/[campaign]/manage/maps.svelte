d86f8ac5916631c67f0194f8746ed98f51fd7b20
Adam Green <adam.green@kcl.ac.uk>
2022-12-23 17:16:17 +0000

    Added map screen

--- src/routes/game/[campaign]/manage/maps.svelte
+++ src/routes/game/[campaign]/manage/maps.svelte
@@ -8,7 +8,7 @@
 <div class="w-screen h-screen absolute top-0 left-0 flex items-center justify-center pointer-events-none">
     <div class="max-w-6xl flex flex-wrap pointer-events-auto max-h-[70vh] overflow-y-auto px-2">
         {#each game.campaign.maps as map}
-            <div class="basis-1/3">
+            <div class="basis-1/3 flex">
                 <Button on:click={() => changeMap(map.id)}>
                     <img src={map.main.url} alt={map.main.alt} />
                     <h2>{map.name}</h2>
