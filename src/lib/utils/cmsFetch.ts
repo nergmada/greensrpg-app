b91c6da7d011f7489a974a2f76ad51ebb2667805
Adam Green <adam.green@kcl.ac.uk>
2022-12-23 16:59:42 +0000

    Added console.log for errors

--- src/lib/utils/cmsFetch.ts
+++ src/lib/utils/cmsFetch.ts
@@ -1,24 +1,22 @@
 import axios from 'axios';
 
 import { PUBLIC_CMS_URL } from '$env/static/public';
-import { errors } from '$lib/data/errors';
 
 export async function get<T extends GetEndpoint>(
 	endpoint: T,
 	query: string,
 	substitutes: Record<string, string> = {}
 ): Promise<CMSResponseType<T> | null> {
-	console.log(PUBLIC_CMS_URL);
 	const subEndpoint = Object.keys(substitutes).reduce(
 		(prev, k) => prev.replace(`{${k}}`, substitutes[k]),
 		endpoint
 	);
 	const url = `${PUBLIC_CMS_URL}${subEndpoint}`;
-	console.log(url);
 	try {
 		const { data } = await axios.get(url);
 		return data;
 	} catch (err) {
+		console.log(err);
 		// errors.update((v) => [...v, err]);
 		return null;
 	}
